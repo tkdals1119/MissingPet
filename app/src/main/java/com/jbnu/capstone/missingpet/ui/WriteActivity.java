099b469937ebb339eb0dd50e59c46593875edf0a
unknown <asb1119@naver.com>
2017-12-05 18:25:47 +0900

    사진작업중2

--- app/src/main/java/com/jbnu/capstone/missingpet/ui/WriteActivity.java
+++ app/src/main/java/com/jbnu/capstone/missingpet/ui/WriteActivity.java
@@ -1,7 +1,6 @@
 package com.jbnu.capstone.missingpet.ui;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -29,7 +28,6 @@ import com.jbnu.capstone.missingpet.service.DB_Manger;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -38,7 +36,6 @@ import butterknife.ButterKnife;
 
 public class WriteActivity extends AppCompatActivity {
 
-    final int REQ_CODE_SELECT_IMAGE=100;
     private static final String TAG = "@@@@@@@@@@@@@";
     @BindView(R.id.taken_image) ImageView taken_image;
 
@@ -63,6 +60,12 @@ public class WriteActivity extends AppCompatActivity {
         menuInflater.inflate(R.menu.menu, menu);
         return true;
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        taken_image.setImageURI(data.getData());
+    }
+
     //추가된 소스, ToolBar에 추가된 항목의 select 이벤트를 처리하는 함수
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -71,31 +74,23 @@ public class WriteActivity extends AppCompatActivity {
             case R.id.addfile:
                 // User chose the "Settings" item, show the app settings UI...
                 Toast.makeText(getApplicationContext(), "파일 추가 버튼", Toast.LENGTH_LONG).show();
+
+
                 DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                        if (ContextCompat.checkSelfPermission(WriteActivity.this, Manifest.permission.CAMERA)
-                                != PackageManager.PERMISSION_GRANTED)
-                        {
-                            ActivityCompat.requestPermissions(WriteActivity.this,
-                                    new String[]{Manifest.permission.CAMERA},
-                                    0);
-                        }
-                        Log.d(TAG, "@@@@@@@@@@@@@@@@@@@startActivityForResult");
-
-                        startActivityForResult(intent, 0);
+                        startActivityForResult(intent, 1);
                     }
                 };
+
+
                 DialogInterface.OnClickListener albumlistener = new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent intent = new Intent(Intent.ACTION_PICK);
-                        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-                        intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                        startActivityForResult(intent, REQ_CODE_SELECT_IMAGE);
                     }
+
+
                 };DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
@@ -109,8 +104,6 @@ public class WriteActivity extends AppCompatActivity {
                         .show();
                 return true;
 
-
-
             default:
                 // If we got here, the user's action was not recognized.
                 // Invoke the superclass to handle it.
@@ -118,35 +111,12 @@ public class WriteActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+//    {
+//        String path = "/Pictures/Instagram/IMG_20170818_155008_406.jpg";
+//        taken_image.setImageURI(Uri.parse(path));
+//    }
 
-    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Toast.makeText(getBaseContext(), "resultCode : "+resultCode,Toast.LENGTH_SHORT).show();
-        if(requestCode == REQ_CODE_SELECT_IMAGE)
-        {
-            if(resultCode== Activity.RESULT_OK)
-            {
-                try
-                {
-                    //Uri에서 이미지 이름을 얻어온다.
-                    // String name_Str = getImageNameToUri(data.getData());
-                    // 이미지 데이터를 비트맵으로 받아온다.
-                     Bitmap image_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
-                     ImageView image = (ImageView)findViewById(R.id.taken_image);
-                     //배치해놓은 ImageView에 set image.setImageBitmap(image_bitmap);
-
-                    // Toast.makeText(getBaseContext(), "name_Str : "+name_Str , Toast.LENGTH_SHORT).show();
-                     } catch (FileNotFoundException e) {
-                            // TODO Auto-generated catch block
-                             e.printStackTrace();
-                      } catch (IOException e) {
-                            // TODO Auto-generated catch block
-                             e.printStackTrace();
-                } catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-                }
-            }
-        }
-    }
-
+}
