a69be843ec76e79b47b72ebe0bed3458f94027d7
tkdals1119 <asb1119@naver.com>
2017-12-23 02:51:15 +0900

    다시시작

--- app/src/main/java/com/jbnu/capstone/missingpet/DetailViewActivity.java
+++ app/src/main/java/com/jbnu/capstone/missingpet/DetailViewActivity.java
@@ -156,21 +156,21 @@ public class DetailViewActivity extends AppCompatActivity {
 
 
     //추가된 소스, ToolBar에 추가된 항목의 select 이벤트를 처리하는 함수
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        //return super.onOptionsItemSelected(item);
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                // User chose the "Settings" item, show the app settings UI...
-                Toast.makeText(getApplicationContext(), "새로고침", Toast.LENGTH_LONG).show();
-                return true;
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                Toast.makeText(getApplicationContext(), "나머지 버튼 클릭됨", Toast.LENGTH_LONG).show();
-                return super.onOptionsItemSelected(item);
-        }
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        //return super.onOptionsItemSelected(item);
+//        switch (item.getItemId()) {
+//            case R.id.action_settings:
+//                // User chose the "Settings" item, show the app settings UI...
+//                Toast.makeText(getApplicationContext(), "새로고침", Toast.LENGTH_LONG).show();
+//                return true;
+//            default:
+//                // If we got here, the user's action was not recognized.
+//                // Invoke the superclass to handle it.
+//                Toast.makeText(getApplicationContext(), "나머지 버튼 클릭됨", Toast.LENGTH_LONG).show();
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 
 
 }
