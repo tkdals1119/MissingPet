d2309bbcdf7e8becdf339d433a8307ff11bb81f5
tkdals1119 <asb1119@naver.com>
2018-01-03 03:07:03 +0900

    [추가] WriteActivity add Toolbar icon(send_icon)

--- app/src/main/java/com/jbnu/capstone/missingpet/ui/WriteActivity.java
+++ app/src/main/java/com/jbnu/capstone/missingpet/ui/WriteActivity.java
@@ -244,18 +244,16 @@ public class WriteActivity extends AppCompatActivity {
         return true;
     }
 
+    //추가된 소스, ToolBar에 추가된 항목의 select 이벤트를 처리하는 함수
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //return super.onOptionsItemSelected(item);
         switch (item.getItemId()) {
             case R.id.send:
-                // User chose the "Settings" item, show the app settings UI...
-                Toast.makeText(getApplicationContext(), "환경설정 버튼 클릭됨", Toast.LENGTH_LONG).show();
-                return true;
+                Toast.makeText(getApplicationContext(), "전송 버튼asdf", Toast.LENGTH_LONG).show();
+                return super.onOptionsItemSelected(item);
+
             default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                Toast.makeText(getApplicationContext(), "나머지 버튼 클릭됨", Toast.LENGTH_LONG).show();
                 return super.onOptionsItemSelected(item);
         }
     }
